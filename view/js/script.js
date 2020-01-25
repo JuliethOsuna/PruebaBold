593ccca9e4919efaf34f9a82611fcbc28f1a2d6e
JuliethOsuna <juliosu0324@gmail.com>
2020-01-25 12:58:45 -0500

    test

--- view/js/script.js
+++ view/js/script.js
@@ -1,24 +1,64 @@
 function Weather() {
+  this.timeout;
 }
 
 Weather.prototype.fetchResults = function(val) {
-  
+  return fetch(`https://jsonmock.hackerrank.com/api/weather?name=${val}`)
+  .then(function(response) {
+    return response.json();
+  });
 }
 
 Weather.prototype.onKeyup = function(e) {
-  
+  console.log(e.target.value);
+
+  clearTimeout(this.timeout);
+
+  this.timeout = setTimeout(() => {
+    this.fetchResults(e.target.value).then((res) => {
+      this.weatherResults = res.data;
+      this.updateSuggestions();
+    })
+  }, 1000);
 }
 
 Weather.prototype.updatecitySelect = function(results) {
-  
+
 }
 
 Weather.prototype.updateSuggestions = function() {
-  
+  this.$suggestions.innerHTML= '';
+  if(!this.weatherResults.length) {
+    const suggestion = document.createElement('div');
+    suggestion.classList.add('suggestionItem');
+    suggestion.appendChild(document.createTextNode('No results'));
+    suggestion.style.color='red';
+    this.$suggestions.appendChild(suggestion);
+
+  }
+  this.weatherResults.forEach((item) => {
+    const suggestion = document.createElement('div');
+    suggestion.classList.add('suggestionItem');
+    suggestion.appendChild(document.createTextNode(item.name));
+    suggestion.addEventListener("click", () => {
+      console.log(item)
+      this.$selectedCity.innerHTML = item.name;
+      this.$selctedWeather.innerHTML = item.weather;
+      this.$selectedStatus.innerHTML = item.status;
+      this.$city.value = item.name;
+    })
+    this.$suggestions.appendChild(suggestion);
+  })
 }
 
 Weather.prototype.reset = function() {
-  
+  this.$selectedCity.innerHTML = '';
+  this.$selctedWeather.innerHTML = '';
+  this.$selectedStatus.innerHTML = '';
+  this.$city.value= '';
+  this.$suggestions.innerHTML= '';
+
+
 }
 
 Weather.prototype.init = function() {
